029c46a17f2e9bc3198e1a2a0915fab002611200
Mehran Mazhar <mehran.mazhar@gmail.com>
2022-05-13 22:12:45 +0430

    f

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,8 +1,8 @@
 use super::method::Method;
 use std::error::Error;
-use std::str; 
-use std::str::{Utf8Error};
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str;
+use std::str::Utf8Error;
 
 pub struct Request {
     path: String,
@@ -13,18 +13,28 @@ pub struct Request {
 impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
+    // Get /search?name=abc&sort=1 HTTP/1.1 \r\n Headers
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf)?;
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+        
         unimplemented!();
     }
 }
 
-fn get_next_word(request:&str) ->Option<(&str,&str)> {
-    for (i,v) in request.chars().enumerate(){
-        if v == ' ' {
-            return Some((&request[..i],&request[i+1..]));
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, v) in request.chars().enumerate() {
+        if v == ' ' || v == '\r' {
+            return Some((&request[..i], &request[i + 1..]));
         }
-    }    
+    }
 
     return None;
 }
@@ -47,8 +57,8 @@ impl ParseError {
     }
 }
 
-impl From<Utf8Error> for ParseError{
-    fn from(_: Utf8Error) -> Self{
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
         Self::InvalidEncoding
     }
 }
