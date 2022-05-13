e1b00bf50eb9ca3f4e1cfddbb3dbd8700c82f4e4
Mehran Mazhar <mehran.mazhar@gmail.com>
2022-05-13 20:52:15 +0430

    INIT

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use server::Server;
+use http::Request;
+use http::Method;
+
+mod server;
+mod http;
+
+fn main() {
+    let server = Server::new("127.0.0.1:8090".to_string());
+    server.run();
+}
